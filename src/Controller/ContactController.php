f9f96d8517638e5814cd0a3e85a6ad6a0bf13b6b
Fr-JuJu <juliesueur70@gmail.com>
2024-09-16 11:40:22 +0200

    mailhog

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,33 +3,50 @@
 namespace App\Controller;
 
 use App\Entity\Contact;
-use App\Form\DemoFormType;
 use App\Form\ContactFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    public function index(Request $request, EntityManagerInterface $entityManager, MailerInterface $mailer): Response
     {
-        $form = $this->createForm(ContactFormType::class);
+        $contact = new Contact();
+        $form = $this->createForm(ContactFormType::class, $contact);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
+        
 
-            //on crée une instance de Contact
-            $message = new Contact();
-            // Traitement des données du formulaire
-            $data = $form->getData();
-            //on stocke les données récupérées dans la variable $message
-            $message = $data;
+            if($form->isSubmitted() && $form->isValid()){
+                $contact = $form->getData();
+                $entityManager->persist($contact);
+                $entityManager->flush();
 
-            $entityManager->persist($message);
-            $entityManager->flush();
+                // Mail à partir d'ici
+                $email = (new Email())
+                ->from('hello@example.com')
+                ->to('you@example.com')
+                //->cc('cc@example.com')
+                //->bcc('bcc@example.com')
+                //->replyTo('fabien@example.com')
+                //->priority(Email::PRIORITY_HIGH)
+                ->subject('Time for Symfony Mailer !')
+                ->text('Sending emails is fun again ! (NOOOOO !!!!!)')
+                ->html('<p>See Twig integration for more examples</p>');
+
+                try {
+                    $mailer->send($email);
+                } catch (TransportExceptionInterface $e) {
+                    // some error prevented the email sending; display an
+                    // error message or try to resend the message
+                }
 
             // Redirection vers accueil
             return $this->redirectToRoute('app_accueil');
